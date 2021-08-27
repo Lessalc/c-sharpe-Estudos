07554e66c9e3eff2d6a0095417a737a8ff9dda5e
Lessalc <lucianoclessa@gmail.com>
2021-08-27 01:08:52 -0300

    Adicionando desafios DIO e alteracoes no projeto Revisao

---
+++ desafios/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace desafios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Multiplos();
+            // NumerosImpares();
+            // ConversaoTempo();
+            TrabalhoElfos();
+        }
+
+
+
+
+        private static void Multiplos()
+        {
+            string[] valores = Console.ReadLine().Split(' ');
+            int A = int.Parse(valores[0]);
+            int B = int.Parse(valores[1]);
+
+            if (A % B == 0 || B % A ==0 ) {
+                Console.WriteLine("Sao Multiplos");
+            }
+            else {
+                Console.WriteLine("Nao sao Multiplos");
+        
+            }
+        }
+
+        private static void NumerosImpares()
+        {
+            int n;
+            n = int.Parse(Console.ReadLine());
+            //complete seu codigo
+            for (int i=1; i<=n; i+=2) //complete seu codigo
+            {
+                Console.WriteLine(i);
+            }
+            
+            Console.ReadLine();
+        }
+
+        private static void ConversaoTempo()
+        {
+            var timeInSeconds = int.Parse(Console.ReadLine());
+
+            //continue a solucao
+            var hours = timeInSeconds / (60*60);
+            timeInSeconds =  timeInSeconds % (60*60);
+            var minutes = timeInSeconds / 60;
+            var seconds = timeInSeconds % 60;
+
+            Console.WriteLine($"{hours}:{minutes}:{seconds}");
+        }
+
+        private static void TrabalhoElfos()
+        {
+            string[] line1 = Console.ReadLine().Split(" ");
+            string[] line2 = Console.ReadLine().Split(" ");
+            
+            int finalExpediente = int.Parse(line1[0]);
+            
+            int presente1 = int.Parse(line2[0]);
+            int presente2 = int.Parse(line2[1]);
+            
+            //continue a solucao
+            int totalNecessario =  presente1 + presente2;
+            
+            if (totalNecessario > finalExpediente)
+            {
+            Console.WriteLine("Deixa para amanha!");
+            }
+            else
+            {
+            Console.WriteLine("Farei hoje!");
+             }
+        }
+
+    }
+
+    
+}
+
+
---
+++ desafiosnumericos/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace desafiosnumericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TipoCombustivel();
+            // OMaior();
+            ValidacaoNota();
+        }
+
+        private static void ValidacaoNota()
+        {
+            int contador=0;
+            double soma=0;
+            while(contador <2)
+            {
+                double valueRead = Convert.ToDouble(Console.ReadLine());
+                if(valueRead>=0 && valueRead<=10){
+                    soma += valueRead;
+                    contador++;
+                }
+                else{
+                    Console.WriteLine("nota invalida");
+                }
+            }
+            Console.WriteLine("media = {0:0.00}",(soma/contador));
+
+        }
+
+        private static void OMaior()
+        {
+            string[] vet = Console.ReadLine().Split(' ');
+            //complete declarando as variáveis
+
+            int MAIORAB = (Int32.Parse(vet[0]) + Int32.Parse(vet[1]) + Math.Abs(Int32.Parse(vet[0])-Int32.Parse(vet[1])))/2;
+
+            int MAIOR = (MAIORAB+ Int32.Parse(vet[2]) + Math.Abs(MAIORAB - Int32.Parse(vet[2]))) / 2;
+
+            Console.WriteLine($"{MAIOR} eh o maior");
+        }
+
+
+        private static void TipoCombustivel()
+        {
+            int opcao;
+            int alcool = 0, gasolina = 0, diesel = 0;
+            do
+            {
+                opcao = Convert.ToInt32(Console.ReadLine());    
+                switch (opcao)
+                {
+                    case 1:
+                        alcool++;
+                        break;
+                    case 2:
+                        gasolina++;    
+                        break;
+                    case 3:
+                        diesel++;
+                        break;
+                    case 4:
+                        Console.WriteLine("MUITO OBRIGADO");
+                        Console.WriteLine("Alcool: "+alcool);
+                        Console.WriteLine("Gasolina: "+gasolina);
+                        Console.WriteLine("Diesel: "+diesel);
+                        break;
+                    default:                       
+                        continue;
+                }
+            }
+       
+            while (opcao != 4);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
